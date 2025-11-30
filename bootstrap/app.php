5bd0088d5540911e3a289d79b32cbb30a9d0e3d1
Vin Tristan E. Balajadia <balajadiavintristan@gmail.com>
2025-11-30 19:24:33 +0800

    Intial commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +11,24 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'student.auth' => \App\Http\Middleware\StudentAuth::class,
+            'admin.auth' => \App\Http\Middleware\AdminAuth::class,
+            'admin.role' => \App\Http\Middleware\CheckAdminRole::class,
+            'request.ownership' => \App\Http\Middleware\CheckRequestOwnership::class,
+            'throttle.requests' => \App\Http\Middleware\ThrottleRequest::class,
+            'log.activity' => \App\Http\Middleware\LogRequestActivity::class,
+        ]);
+
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('admin') || $request->is('admin/*')) {
+                return route('admin.login');
+            }
+            
+            return route('student.login');
+        });
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
